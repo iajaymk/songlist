422fb31d3b76ba186f79f2b4306ca3ba8fc1207e
Ajay Kumar <ajaykumarmkaju@gmail.com>
2022-07-28 04:07:10 +0530

    Add Songs, Artists, People models

--- songs/admin.py
+++ songs/admin.py
@@ -1,3 +1,17 @@
+from re import S
 from django.contrib import admin
+from .models import People, Artists, Songs
 
-# Register your models here.
+
+@admin.register(Songs)
+class SongsAdmin(admin.ModelAdmin):
+    list_display = ['song_name','cover', 'release_date']
+
+
+@admin.register(People)
+class PeopleAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email']
+
+@admin.register(Artists)
+class ArtistsAdmin(admin.ModelAdmin):
+    list_display = ['artist_name','dob','bio']
--- songs/models.py
+++ songs/models.py
@@ -1,3 +1,39 @@
+from distutils.command.upload import upload
+from email.policy import default
 from django.db import models
 
 # Create your models here.
+
+class People(models.Model):
+    email = models.EmailField()
+    name = models.CharField(max_length=50)
+
+    class Meta: 
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
+
+class Artists(models.Model):
+    artist_name = models.CharField(max_length=50)
+    dob = models.DateField()
+    bio = models.TextField()
+
+    class Meta:
+        ordering = ('artist_name',)
+
+    def __str__(self):
+        return self.artist_name
+
+
+class Songs(models.Model):
+    song_name = models.CharField(max_length = 25)
+    release_date = models.DateField()
+    artist = models.ManyToManyField('Artists', related_name='songs')
+    cover = models.ImageField(upload_to='uploads/', default="uploads/default_music.jpeg")
+
+    class Meta:
+        ordering=('song_name',)
+
+    def __str__(self):
+        return self.song_name
